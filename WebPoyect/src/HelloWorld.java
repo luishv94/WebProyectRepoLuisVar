45367f2cb11caf94105c34a65137e3726cd1e7db
Luis <luishv94@gmail.com>
2014-05-28 08:22:41 -0500

    Primera Version

---
+++ WebPoyect/src/HelloWorld.java
@@ -0,0 +1,23 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException{
+		response.setContentType("text/html");
+		PrintWriter writer=response.getWriter();
+		writer.print("<html><body><h1>Hola Mundo</h1>"+new Date().toString()+"</body></html>");
+	}	
+
+}
