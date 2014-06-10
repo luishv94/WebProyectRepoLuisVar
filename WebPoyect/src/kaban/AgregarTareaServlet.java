142c57d295fe3cc427a17984340e5e2417c341a0
Luis <luishv94@gmail.com>
2014-06-10 11:51:35 -0500

    Add task without state&priority

--- WebPoyect/src/kaban/AgregarTareaServlet.java
+++ WebPoyect/src/kaban/AgregarTareaServlet.java
@@ -1,7 +1,5 @@
 package kaban;
 
-//Import classes Tarea
-
 
 
 
@@ -14,6 +12,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import kaban.Programa;
+import kaban.State;
+import kaban.Tarea;
+
 /**
  * Servlet implementation class AgregarTareaServlet
  */
@@ -23,7 +25,7 @@ public class AgregarTareaServlet extends HttpServlet {
    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
-		PrintWriter  writer = response.getWriter();
+		PrintWriter writer= response.getWriter();
 		writer.print("<html>"
 				+"<head>"
 				+"<title>Add task</title>"
@@ -34,44 +36,47 @@ public class AgregarTareaServlet extends HttpServlet {
 				+"<tr>"
 				+"<td>Titulo:</td>"
 				+"<td>"
-				+"<input type=\"Text\" name\"titulo\">"
+				+"<input type=\"text\" name=\"Titulo\"/>"
 				+"</td>"
 				+"</tr>"
 				+"<td>Categoria:</td>"
 				+"<td>"
-				+"<input type=\"Text\" name\"Categoria\">"
+				+"<input type=\"text\" name=\"Categoria\"/>"
 				+"</td>"
 				+"</tr>"
 				+"<td>Propietario:</td>"
 				+"<td>"
-				+"<input type=\"Text\" name\"Propietario\">"
+				+"<input type=\"text\" name=\"Propietario\"/>"
 				+"</td>"
 				+"</tr>"
 				+"<td>Descripcion:</td>"
 				+"<td>"
-				+"<input type=\"Text\" name\"Descripcion\">"
+				+"<input type=\"text\" name=\"Descripcion\"/>"
 				+"</td>"
 				+"</tr>"
 				+"<td>Fecha de Entrega:</td>"
 				+"<td>"
-				+"<input type=\"Text\" name\"FechaDeEntrega\">"
+				+"<input type=\"text\" name=\"FechaDeEntrega\"/>"
 				+"</td>"
 				+"</tr>"
-				+"<tr>"
-				+"<td>Estado</td>"
-				+"<td>"
-				+"<select>"
-				+"<option value=\"BACKLOG\" selected>Backlog</option>"
-				+"<option value=\"TO_DO\">To do</option>"
-				+"<option value=\"IN_PROGRESS\">In Progress</option>"
-				+"<option value=\"DONE\">Done</option>"
-				+"</select>"
-				+"</td>"
-				+"</tr>"
+				
++"<tr>"
++"<td>Estado:</td>"
++"<td>"
++"<select name=\"Estado\">"
++"<option value=\"BACKLOG\"selected>Backlog</option>"
++"<option value=\"TO_DO\">To do</option>"
++"<option value=\"IN_PROGRESS\">In progress</option>"
++"<option value=\"DONE\">done</option>"
++"</select>"	
++"</td>"
++"</tr>"
+
+				
 				+"<tr>"
 				+"<td>Prioridad</td>"
 				+"<td>"
-				+"<select>"
+				+"<select name=\"Prioridad\">"
 				+"<option value=\"Baja\" selected>Baja</option>"
 				+"<option value=\"Media\">Media</option>"
 				+"<option value=\"Alta\">Alta</option>"
@@ -95,15 +100,16 @@ public class AgregarTareaServlet extends HttpServlet {
 			HttpServletResponse response) throws ServletException, IOException {
 
 		Tarea task = new Tarea();
+		PrintWriter writer = response.getWriter();
 
-		task.setTitulo(request.getParameter("titulo"));
+		task.setTitulo(request.getParameter("Titulo"));
 		task.setCategoria(request.getParameter("Categoria"));
 		task.setPropietario(request.getParameter("Propietario"));
 		task.setDescripcion(request.getParameter("Descripcion"));
 		task.setFechaDeEntrega(request.getParameter("FechaDeEntrega"));
 		
 		try {
-			task.setState(State.valueOf(request.getParameter("state")));
+			task.setState(State.valueOf(request.getParameter("Estado")));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -117,8 +123,24 @@ public class AgregarTareaServlet extends HttpServlet {
 		Programa.dashboard.add(task);
 
 		response.setContentType("text/html");
-		PrintWriter writer = response.getWriter();
 		writer.print("<html><body>" + task.toHtml() + "</body></html>");
+		/*writer.print("<html>"
+				+"<body>"
+				+"Title: "+task.getTitulo()
+				+"<br>"
+				+"Description: "+task.getDescripcion()
+				+"<br>"
+				+"Owner: "+task.getPropietario()
+				+"<br>"
+				+"Category: "+task.getCategoria()
+				+"<br>"
+				+"Due Date: "+task.getFechaDeEntrega()
+				+"<br>"
+				+"Priority: "+task.getPrioridad()
+				+"<br>"
+				+"State: "+task.getEstado()
+				+"</body>"
+				+"</html>");*/
 	}
 	
 
